7f540d1ffe65dc2adbbeaf9f5a341ee920ecba5c
Andres Vargas <andvarp@gmail.com>
2018-06-03 15:24:43 -0600

    Deploy client files to heroku

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve production assets from client folder
+  app.use(express.static('client/build'));
+
+  // Serve production index file
+  const path = requiere('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Starts on ${PORT}`));
